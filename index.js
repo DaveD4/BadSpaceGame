440195927107fbf2ee375c1d6c48e1a077704a89
David Mayer <david.schlatt@web.de>
2022-06-29 00:11:59 +0200

    refactoring

--- index.js
+++ index.js
@@ -14,52 +14,7 @@ function draw(){
 
 }
 
-class Ship{
-    constructor({
-        position, 
-        velocity,
-        accelartion, 
-        image
-    })
-    {
-        this.position=position
-        this.velocity=velocity
-        this.accelartion=accelartion
-        this.image=new Image()
-        this.image.src=image.src
-    }
-    update(){
-        this.velocity.x+=this.accelartion.x
-        this.velocity.y+=this.accelartion.y
-        this.velocity.vRotation+=this.accelartion.z
-        
-        this.position.x+=this.velocity.x
-        this.position.y+=this.velocity.y
-        this.position.rotation=(this.position.rotation+this.velocity.vRotation)%(2*Math.PI)
-        console.log(this.position)
-    }
-    draw(){
-        c.save()
-        c.translate(this.position.x+32/2, this.position.y+32/2)
-        c.rotate(this.position.rotation)
-        c.translate(-this.position.x-32/2, -this.position.y-32/2)
-        //c.translate(canvas.width/10,canvas.width/4)
-        c.drawImage(this.image,this.position.x,this.position.y)
-        c.restore()
 
-        
-      
-       
-    }
-    control(thrustVector){
-        const rotationInDegree=this.position.rotation*180/Math.PI
-        const cosPhi=Math.cos(this.position.rotation)
-        const sinPhi=Math.sin(this.position.rotation)
-        this.accelartion.x=cosPhi*thrustVector.x-sinPhi*thrustVector.y
-        this.accelartion.y=sinPhi*thrustVector.x+cosPhi*thrustVector.y 
-        this.accelartion.z=thrustVector.z
-    }
-}
 
 
 //thrustVectorfromInput
@@ -181,5 +136,75 @@ window.addEventListener('keyup',(e)=> {
             break;
     }
     
+  class GameState{
+    constructor(game)
+    {
+        this.game=game
+    }
+    onEntry(){
+        const temp = this.game.test
+        console.log(temp)
+    }
+    onExit(){
+    }
+}
+class StartScreen extends GameState{
+    constructor(game){
+        super(game)
+    }
+    onEntry(){
+        this.run()
+    ctx.fillStyle = 'gray';
+    ctx.fillRect(80, 60, 140, 30);
+    const temp = this.game.test
+    console.log(this.game.test)
+    }
+    onExit(){
+    }
+    run(){
+        ctx.save()
+
+        ctx.restore()
+    }
+}
+class LevelSelectScreen extends GameState{
+    constructor(){
+        super()
+    }
+    onEntry(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(80, 60, 140, 30);
+    }
+}
+class Mission extends GameState{
+    constructor(){
+        super()
+    }
+    onEntry(){}
+}
+
+class Game{
+    constructor()
+    {
+        this.lastState=this.currentState
+        this.test="hello"
+        this.currentState=new StartScreen(this)
+        //this.currentState=new GameState(this)
+    }
+    init(){
+        this.currentState.onEntry()
+        //this.currentState=this.currentState.
+    }
+}
+
+//const game=new Game()
+//game.init()  
     
 })
+
+
+
+
+
+
+
